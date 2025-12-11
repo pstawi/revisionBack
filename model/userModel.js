900b3debae5ca3ae62d02c5496ba6ee2654dc20d
pierreStawi <pstawikowski@fcdigital.fr>
2025-12-11 09:23:59 +0100

    Add updateUser functionality to userController, userModel, and userRoute

--- model/userModel.js
+++ model/userModel.js
@@ -30,3 +30,21 @@ export const getUserByLogin = async (login) => {
     const [result] = await connexion.query(selectUser, [login]);
     return result;
 }
+
+export const updateUser = async (id, prenom, nom, login, password, roleId) => {
+    let updateUserQuery;
+    let params;
+    
+    if (password) {
+        // Si un mot de passe est fourni, on le met à jour aussi
+        updateUserQuery = "UPDATE user SET prenom = ?, nom = ?, login = ?, password = ?, roleId = ? WHERE id = ?";
+        params = [prenom, nom, login, password, roleId, id];
+    } else {
+        // Si pas de mot de passe, on ne met pas à jour ce champ
+        updateUserQuery = "UPDATE user SET prenom = ?, nom = ?, login = ?, roleId = ? WHERE id = ?";
+        params = [prenom, nom, login, roleId, id];
+    }
+    
+    const [result] = await connexion.query(updateUserQuery, params);
+    return result;
+}
--- route/userRoute.js
+++ route/userRoute.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router.post("/register", userController.addUser);
 router.get("/users", auth, checkRole, userController.getAllUsers);
 router.get("/user/:id", auth, userController.getUserById);
+router.put("/user/:id", auth, userController.updateUser);
 router.delete("/user/:id", auth, userController.deleteUser);
 router.post("/login", userController.login);
 
